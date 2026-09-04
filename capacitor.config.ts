929c980e64b2894c4c4cd38929ebb4139be007f0
Caio Abrahão <contato.caioabrahao@gmail.com>
2026-09-04 14:27:03 -0300

    started capacitor config

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'br.com.caioabrahao.cali',
+  appName: 'Cali',
+  webDir: 'dist'
+};
+
+export default config;
